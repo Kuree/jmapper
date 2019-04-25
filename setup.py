4fc9651e85968e18c15b0cc2a7f2f92c9b5a058b
Keyi Zhang <keyi@stanford.edu>
2019-04-24 18:03:11 -0700

    setup things

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='jmapper',
+    packages=[
+        "jmapper"
+    ],
+    version='0.0.3',
+    author='Keyi Zhang',
+    author_email='keyi@stanford.edu',
+    description='Mapper for Jade CGRA',
+    url="https://github.com/Kuree/jmapper",
+    install_requires=[
+        "coreir",
+    ],
+)
