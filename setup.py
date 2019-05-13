dcb2b1a4da119c3f64d4ee86a6118cf88c852e0d
Keyi Zhang <keyi@stanford.edu>
2019-05-13 12:01:29 -0700

    use fixed mantle branch

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.0.4',
+    version='0.0.5',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
