70997ce7f894ad45f423a08e785564bd1c9cfdf0
Keyi Zhang <keyi@stanford.edu>
2019-05-27 19:27:02 -0700

    add correct const duplication

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.0.8',
+    version='0.0.9',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
