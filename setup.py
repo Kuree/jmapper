cba9c492339e15360d5f0f1b4ffbbb1e3b302fcc
Keyi Zhang <keyi@stanford.edu>
2019-05-28 22:49:20 -0700

    fix addr connection

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.0',
+    version='0.1.1',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
