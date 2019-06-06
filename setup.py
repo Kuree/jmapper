7ed283df569ee86d7f9e83c65c5510028abd603e
Keyi Zhang <keyi@stanford.edu>
2019-06-06 13:54:18 -0700

    add abs support

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.2',
+    version='0.1.3',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
