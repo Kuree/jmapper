538b23167c6fc3942d2dd06def7beabd6cf5fb33
Keyi Zhang <keyi@stanford.edu>
2019-06-14 14:47:05 -0700

    add stride 1

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.6',
+    version='0.1.7',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
