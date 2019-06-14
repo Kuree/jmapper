0acab1f8cd60691a9aed6afa9dd191cb0c0ae369
Keyi Zhang <keyi@stanford.edu>
2019-06-14 14:32:16 -0700

    use new coreir ubuffer

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.5',
+    version='0.1.6',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
