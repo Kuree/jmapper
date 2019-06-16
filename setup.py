2be1894f39318115103ee148d555f003a6146639
Keyi Zhang <keyi@stanford.edu>
2019-06-16 15:27:20 -0700

    add ren port

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.10',
+    version='0.1.11',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
