05a755c8de42669185cbcc7e5dfe72354c00b8f9
Keyi Zhang <keyi@stanford.edu>
2019-06-16 18:49:07 -0700

    bump version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=[
         "jmapper"
     ],
-    version='0.1.13',
+    version='0.1.14',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Mapper for Jade CGRA',
